a20758038fee6182ea5353133d161aae5ab3e949
JoseEduardo23 <jose.lincango@epn.edu.ec>
2024-06-01 20:31:54 -0500

    Constructor, Getters, Setters, Metodos de figuras 2D JL

--- src/figurasGeometricas/dosD.java
+++ src/figurasGeometricas/dosD.java
@@ -1,4 +1,44 @@
 package figurasGeometricas;
-
+import figurasGeometricas.FigurasGeometricas;
 public class dosD extends FigurasGeometricas {
+    double tamanio_lados;
+
+    public dosD() {
+    }
+
+    public dosD(int nlados, String nombre) {
+        super(nlados, nombre);
+    }
+
+    public double getTamanio_lados() {
+        return tamanio_lados;
+    }
+
+    public void setTamanio_lados(double tamanio_lados) {
+        this.tamanio_lados = tamanio_lados;
+    }
+
+    public void calcularAreaRegulares() {
+
+    }
+
+    public void calcularPerimetroRegulares() {
+
+    }
+
+    public void calcularVolumenRegulares() {
+
+    }
+
+    public void calcularAreaIrregulares() {
+
+    }
+
+    public void calcularPerimetroIrregulares() {
+
+    }
+
+    public void calcularVolumenIrregulares() {
+
+    }
 }
