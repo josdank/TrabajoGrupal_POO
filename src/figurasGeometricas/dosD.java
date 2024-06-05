94ed4a62d90677bfa30a1aeb3daacb892c1fdf46
JoseEduardo23 <jose.lincango@epn.edu.ec>
2024-06-04 20:53:06 -0500

    .

--- src/figurasGeometricas/dosD.java
+++ src/figurasGeometricas/dosD.java
@@ -19,15 +19,30 @@ public class dosD extends FigurasGeometricas {
     }
 
     public void calcularAreaRegulares() {
-
+        if (nombre.equals("Cuadrado")) {
+            double area = tamanio_lados * tamanio_lados;
+            System.out.println("El area del cuadrado es: "+area);
+        } else if (nombre.equals("Triangulo")) {
+            double area = (base*altura)/2;
+            System.out.println("El area del triangulo es: "+area);
+        } else if (nombre.equals("Circulo")) {
+            double area = pi * (radio*radio);
+        }else {
+            System.out.println("Figura no apta para calculo de area");
+        }
     }
 
     public void calcularPerimetroRegulares() {
-
-    }
-
-    public void calcularVolumenRegulares() {
-
+        if (nombre.equals("Cuadrado")) {
+            double perimetro = 4 * tamanio_lados;
+            System.out.println("El perímetro del cuadrado es: " + perimetro);
+        } else if (nombre.equals("TrianguloEquilatero")) {
+            // Implementa el cálculo del perímetro del triángulo equilátero aquí
+        } else if (nombre.equals("Circulo")) {
+            // Implementa el cálculo del perímetro del círculo aquí
+        } else {
+            System.out.println("Figura no soportada para cálculo de perímetro.");
+        }
     }
 
     public void calcularAreaIrregulares() {
