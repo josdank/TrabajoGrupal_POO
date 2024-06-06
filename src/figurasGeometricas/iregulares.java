03c3247ad13121c04e3720b33dc2a05ac91aaddd
MireyaJO <miregarcia444@gmail.com>
2024-06-05 20:06:15 -0500

    Irregulares-García Mireya

--- src/figurasGeometricas/iregulares.java
+++ src/figurasGeometricas/iregulares.java
@@ -1,4 +1,40 @@
 package figurasGeometricas;
 
+import java.util.Scanner;
+
 public class iregulares extends dosD {
+    double longitudLados;
+
+    //Constructores sin y con parámetros
+    public iregulares() {
+    }
+
+    public iregulares(double longitudLados) {
+        this.longitudLados = longitudLados;
+    }
+
+    public iregulares(int nlados, String nombre, double tamanioLados, double longitudLados) {
+        super(nlados, nombre, tamanioLados);
+        this.longitudLados = longitudLados;
+    }
+
+    //Setters y Getters
+    public double getLongitudLados() {
+        return longitudLados;
+    }
+    public void setLongitudLados(double longitudLados) {
+        this.longitudLados = longitudLados;
+    }
+    //Metodos personalizados
+    public double calcularPerimetro(){
+        double contador = 0;
+        System.out.print("Longitud de cada lado");
+        for (int i=0; i<=nlados; i++){
+            Scanner valores = new Scanner(System.in);
+            System.out.println("Ingrese el valor del lado "+i);
+            double valor = valores.nextDouble();
+            contador+=contador+valor;
+        }
+        return contador;
+    }
 }
